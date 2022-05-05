a70382bf742515981e50be5e1a675780d69a19cd
Loick Hommet <hommet.loick@gmail.com>
2022-05-05 13:57:02 +0200

    Utilisation d'un try catch

---
+++ 09-ERRUR-MIDLLEWARE/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+
+
+function myErrorMiddleware(err, req, res, next){
+	//res.status(500).json({erreur: err.message});
+	res.status(500)
+	res.json({erreur: err.message});
+
+}
+
+
+
+app.get('/', (req, res, next) => {
+	try {
+		throw new Error('ceci est un bug dans la route 1');
+	}catch(err){
+		next(err)
+	}
+	
+})
+app.get('/route2', (req, res, next) => {
+	try {
+		throw new Error('ceci est un bug dans la route 2');
+	}catch(err){
+		next(err)
+	}
+	
+})
+
+app.use(myErrorMiddleware); // midlleware d'erreur a la fin
+
+app.listen(3000);
