3f78d53640ecba8fc506cfd178a24d78670b94d9
Loick Hommet <hommet.loick@gmail.com>
2022-05-05 16:05:43 +0200

    Valider avec joi un formulaire

--- 11-VALIDER-LES-INPUTS-AVEC-JOI/app.js
+++ 11-VALIDER-LES-INPUTS-AVEC-JOI/app.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const db = require("../../db");
-//INSERER EXPRES ASUNC AVENT D'	PPELEER express()
-
 const app = express();
+const Joi = require('joi')
+
+
+
 app.use(express.json()) 
 
 app.post("/names", (req, res) => {
   const paylaod = req.body;
-  db.insertOne(paylaod);
+  schema = Joi.object({
+    nom: Joi.string().min(2).max(50).required()
+  })
+ const {value, error} = schema.validate(paylaod)
+ if(error)  res.status(400).send({erreur: error.details[0].message});
+  db.insertOne(value);
 
   console.log(db.getAll());
-  res.status(201).json(paylaod);
+  res.status(201).json(value);
 });
 
 app.listen(3000);
